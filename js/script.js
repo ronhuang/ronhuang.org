aa04c11308cab443300af7c3e5315264e65c77cc
Ron Huang <ronhuang@gmail.com>
2011-04-09 21:51:22 +0800

    Looks like the Less framework website.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,22 @@
-/* Author: 
+/* Author: Ron Huang
 
-*/
+ */
 
+/* Copied from http://lessframework.com/ */
+/* Support touched class. */
+$(document).ready(
+  function() {
+    $("li a").bind('touchstart',function() {
+      $(this).addClass('touched');
+    });
+    $("li a").bind('touchend',function() {
+      $(this).removeClass('touched');
+    });
+    $("li a").bind('touchcancel',function() {
+      $(this).removeClass('touched');
+    });
+  }
+);
 
 
 
